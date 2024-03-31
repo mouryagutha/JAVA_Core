6ae367a2848f2935d33cf740f581aaa4f34e7d49
mourya kumar <kmouryap@gmail.com>
2024-03-31 22:58:10 +0530

    string

--- src/Strings/Helloworld.java
+++ src/Strings/Helloworld.java
@@ -4,13 +4,13 @@ public class Helloworld {
 
     public  static  void main(String[] args)
     {
-        StringBuffer s = new StringBuffer("mourya");
-        s.append("kumar");
+        StringBuffer s = new StringBuffer("hello");
+        s.append("world");
         System.out.println(s);
 
 
-        String sb = new String("mourya");
-        sb.concat("kumar");
+        String sb = new String("hello ");
+        sb.concat("world");
         System.out.println(sb);
     }
 

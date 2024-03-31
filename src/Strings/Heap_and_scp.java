37faf5d9edc8854807f27ac9d254e5cca8d77af0
mourya kumar <kmouryap@gmail.com>
2024-03-31 23:53:33 +0530

    heap and scp

---
+++ src/Strings/Heap_and_scp.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class Heap_and_scp {
+    public static void main(String[] args)
+    {
+        String s1 = new String("spring");
+        s1.concat("fall");
+        String s2 = s1.concat("winter");
+        s2.concat("summer");
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}

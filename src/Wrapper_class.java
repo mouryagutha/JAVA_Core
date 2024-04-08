4c50a453673c46b30a037be044e26b56eae11025
mourya kumar <kmouryap@gmail.com>
2024-04-08 23:46:11 +0530

    wrapper class

---
+++ src/Wrapper_class.java
@@ -0,0 +1,13 @@
+public class Wrapper_class {
+    public static void main(String[] args) {
+        int num = 7 ;
+        Integer num1  = num;
+         System.out.println(num1);
+
+         String str = "34";
+         int num2 = Integer.parseInt(str);
+
+         System.out.println(str);
+         System.out.println(num2*2);
+    }
+}
